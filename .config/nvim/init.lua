68d4b19bfccadb906e8d9277168db72c31c4d091
keskydi <antoine.orhant@gmail.com>
2025-03-26 15:21:48 +0100

    feat(lsp): goto_type_definition

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -9,6 +9,7 @@ vim.g.mapleader = ','
 vim.o.number = true -- Show line number on the side
 vim.o.relativenumber = true
 vim.o.tabstop = 4
+vim.o.expandtab = true
 vim.o.shiftwidth = 4
 --vim.o.ai = true --Auto indent
 --vim.o.si = true --Smart indent
@@ -117,6 +118,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
 		local opts = { buffer = ev.buf }
 		vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
 		vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+		vim.keymap.set('n', 'gy', vim.lsp.buf.type_definition, opts)
 		vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
 		vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
 		-- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
